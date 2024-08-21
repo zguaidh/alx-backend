6700d6795e7b7aa276053509bde361e34e26bb56
zguaidh <zguaidhabiba@gmail.com>
2024-08-21 22:22:44 +0000

    tasks

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -13,20 +13,24 @@ class MRUCache(BaseCaching):
         """Initialize"""
         super().__init__()
         self.cache_data = OrderedDict()
+        self.most_recent_key = None
 
     def put(self, key, item):
         """Adds an item in the cache"""
         if key and item:
             if key in self.cache_data:
-                self.cache_data.move_to_end(key)
-            self.cache_data[key] = item
-            if len(self.cache_data) > self.MAX_ITEMS:
-                discarded, _ = self.cache_data.popitem(last=True)
-                print(f"DISCARD: {discarded}")
+                self.cache_data[key] = item
+            else:
+                if len(self.cache_data) >= self.MAX_ITEMS:
+                    if self.most_recent_key:
+                        discarded = self.most_recent_key
+                        del self.cache_data[discarded]
+                        print(f"DISCARD: {discarded}")
+                self.cache_data[key] = item
 
     def get(self, key):
         """Gets an item by its key"""
         if key in self.cache_data:
-            self.cache_data.move_to_end(key)
+            self.most_recent_key = key
             return self.cache_data[key]
         return None
