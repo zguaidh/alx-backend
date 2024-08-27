160122a744690073f9b950ae0896e28adeeb1099
zguaidh <zguaidhabiba@gmail.com>
2024-08-27 22:11:30 +0000

    task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,10 +4,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     """Returns the template"""
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run()
