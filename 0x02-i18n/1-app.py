1c9bb2755c23920bc089dc1e7b79b5fe57b48619
zguaidh <zguaidhabiba@gmail.com>
2024-08-27 22:16:32 +0000

    task 0

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Module for 1-app"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """Returns template"""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
