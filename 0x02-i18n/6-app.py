db7c25837cd78fd5532439514ac47524e3088733
zguaidh <zguaidhabiba@gmail.com>
2024-08-28 00:01:30 +0000

    task 6

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -33,8 +33,7 @@ def get_locale() -> str:
         locale = request.args.get('locale')
         if locale in app.config['LANGUAGES']:
             return locale
-    elif g.user and g.user.get('locale')
-    in app.config['LANGUAGES']:
+    elif g.user and g.user.get('locale') in app.config['LANGUAGES']:
         return g.user.get('locale')
     else:
         return request.accept_languages.best_match(app.config['LANGUAGES'])
